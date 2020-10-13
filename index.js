c5a5897b7440bf6f80d21f9b3ab95cfd5b83cdc2
Mark Hernandez <m.hern1415@gmail.com>
2020-10-13 21:59:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
 
+let min = array[0]
+let index =  0
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] < min) {
+    array[i] = min
+    index = i
+  }
+}
+
+array.splice(index, 1)
+return min
+
 }
 
 function selectionSort(array){
-
+ let newMin;
+ let sorted = []
+  while(array.length != 0){
+   newMin = findMinAndRemove(array)
+   sorted.push(newMin)
+  }
+return sorted
 }
