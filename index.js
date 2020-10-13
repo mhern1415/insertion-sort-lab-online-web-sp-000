b484369e6d70b6646910deb90235049ae52cb8a0
Mark Hernandez <m.hern1415@gmail.com>
2020-10-13 22:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function minAndRemove(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
 function findMinAndRemove(array){
 
 let min = array[0]
