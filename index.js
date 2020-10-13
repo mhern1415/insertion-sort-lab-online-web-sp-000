3ff2d371791bec7c86fab51b7fd153526112c9f0
Mark Hernandez <m.hern1415@gmail.com>
2020-10-13 22:04:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-function findMinAndRemove(array) {
-  let min = array[0];
-  let minIndex = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-      minIndex = i;
-    }
+function findMinAndRemove(array){
+
+let min = array[0]
+let index =  0
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] < min) {
+    array[i] = min
+    index = i
   }
-  array.splice(minIndex, 1);
-  return min;
+}
+
+array.splice(index, 1)
+return min
+
 }
 
 function selectionSort(array){
